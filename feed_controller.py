71b5cc9bdf78ec73ebc997dbadeedc948e6a3f6a
foo <katie@foo.(none)>
2012-11-26 19:54:05 +0000

    looks much nicer

--- feed_controller.py
+++ feed_controller.py
@@ -38,58 +38,54 @@ def fetch_feeds():
     for s in streams:
         sets_of_feeds.append( SetOfFeeds(s['stream_name'], s['feeds']) )
 
-    # Nav
-    nav_items = ""
-    for s in sets_of_feeds:
-        nav_items += """
-
-    <li><a href='%s'>%s</a></li>""" % (s.get_filename(), s.title)
-    nav = nav_template.render({'nav_items': nav_items})
+    settings_main_content = settings_main_content_foo
+    settings_js = "<script type='text/javascript' src='%sjs/settings.js'></script>" % MEDIA_DIR
+    index_main_content = ""
 
     # The main bit: the actualy feeds on each stream/page
-    items_html = ""
     for s in sets_of_feeds:
+
+        # Construct the nav
+        nav_items = ""
+        for sfoo in sets_of_feeds:
+            if s.get_filename() == sfoo.get_filename():
+                nav_items += """
+                <li class='current'><a href='%s'>%s</a></li>""" % (sfoo.get_filename(), sfoo.title)
+            else:
+                nav_items += """
+                <li><a href='%s'>%s</a></li>""" % (sfoo.get_filename(), sfoo.title)    
+        nav = nav_template.render({'nav_items': nav_items})
+
+
+
+        items_html = ""
         parsed_datums = feed_sifter.feedcache_foo(s.get_urls())
         entry_info_objects = feed_sifter.get_info(parsed_datums)
         for e in entry_info_objects:
             items_html += item_template.render( e.get_dict() )
-        output_html = page_template.render({'main_content':items_html, 'title':s.title, 'nav':nav, 'javascript':''})
+        output_html = page_template.render({'main_content':items_html, 'title':s.title, 'nav':nav, 'settings_nav':settings_nav, 'javascript':''})
 
         f = open(s.get_filename(), 'w')
         f.write(output_html.encode('utf-8'))
         f.close()
         print "%s written." % s.get_filename()
 
-
-    settings_js = "<script type='text/javascript' src='%sjs/settings.js'></script>" % MEDIA_DIR
-    settings_main_content = ""
-    index_main_content = ""
-
-
-
-
-    for s in sets_of_feeds:
         settings_main_content += stream_template.render({'stream_title':s.title, 'stream_url':s.get_filename(), 'stream_feeds':s.feeds_list})
         index_main_content += index_template.render({'stream_title':s.title, 'stream_url':s.get_filename(), 'stream_feeds':s.feeds_list})
 
     settings_main_content += "</div> <!--#streamGoHere-->"
-
     settings_main_content += "<button onclick='addStream()'>Create a new stream</button>"
     settings_main_content += "<button class='bigButton' onclick='saveAllChanges()'>SAVE ALL CHANGES</button>"
+    settings_html = page_template.render({'main_content':settings_main_content, 'title':'Home', 'nav':nav, 'settings_nav':settings_nav,  'javascript':settings_js})
 
-    settings_html = page_template.render({'main_content':settings_main_content, 'title':'Home', 'nav':nav, 'javascript':settings_js})
-
-
+    index_html = page_template.render({'main_content':index_main_content, 'title':'Home', 'nav':'', 'settings_nav':settings_nav, 'javascript':''})
     appearance_js = "<script type='text/javascript' src='%sjs/appearanceSettings.js'></script>" % MEDIA_DIR
-
-    index_html = page_template.render({'main_content':index_main_content, 'title':'Home', 'nav':nav, 'javascript':''})
-    appearance_html = page_template.render({'main_content':appearance_main_content, 'title':'Appearance', 'nav':nav, 'javascript':appearance_js})
+    appearance_html = page_template.render({'main_content':appearance_main_content, 'title':'Appearance', 'nav':nav, 'settings_nav':settings_nav,  'javascript':appearance_js})
 
 
     f = open('%sindex.html' % OUTPUT_DIR, 'w')
     f.write(index_html)
     f.close()
-
 
     f = open('%ssettings.html' % OUTPUT_DIR, 'w')
     f.write(settings_html)
--- settings.py
+++ settings.py
@@ -8,12 +8,13 @@ MEDIA_DIR = "/home/katie/prog/foofurple_feeds/media/"
 TEMPLATES_DIR = "/home/katie/prog/foofurple_feeds/templates/"
 
 nav_template = Template( open('%snav' % TEMPLATES_DIR,'r').read() )
+settings_nav = open('%ssettings_nav' % TEMPLATES_DIR,'r').read()
 item_template = Template( open('%sitem' % TEMPLATES_DIR,'r').read() )
 page_template = Template( open('%spage' % TEMPLATES_DIR,'r').read() )
 list_template = Template( open('%slist' % TEMPLATES_DIR,'r').read() )
 stream_template = Template( open('%sstream' % TEMPLATES_DIR,'r').read() )
 index_template = Template( open('%sindex' % TEMPLATES_DIR,'r').read() )
-settings_main_content_template = Template( open('%ssettings_main_content' % TEMPLATES_DIR,'r').read() )
+settings_main_content_foo = open('%ssettings_main_content' % TEMPLATES_DIR,'r').read()
 
 appearance_main_content = open('%sappearance_main_content' % TEMPLATES_DIR,'r').read()
 
